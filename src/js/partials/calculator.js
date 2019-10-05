a4718b2c13c407c2a0a9aa06e50558195b727b78
romanmoskvin <12345>
2019-10-05 13:44:51 +0300

    блок поясной NAN

--- src/js/partials/calculator.js
+++ src/js/partials/calculator.js
@@ -3,41 +3,49 @@ $(document).ready(function () {
 	var absouluteFloor1 = $(".absoulute-floor-1");
 	var absouluteFloor2 = $(".absoulute-floor-2");
 	var absouluteFloor3 = $(".absoulute-floor-3");
-
 	var floorCount = $(".floor-count");
-
-
-		floorCount.on('change', function() {
-			var result = $(this).val();
-			if(result == 1) {
-				absouluteFloor2.css('display', 'none');
-				absouluteFloor3.css('display', 'none');
-			}
-			else if(result == 2) {
-				absouluteFloor2.css('display', 'block');
-			}
-			else if(result == 3) {
-				absouluteFloor2.css('display', 'block');
-				absouluteFloor3.css('display', 'block');
-			}
-		});
 	
-		$(".calculator_btn").on("click", function (){
-			var houseType = $('input[name="type"]:checked').val();
-			if(houseType == 'type-1') {
-				var floorHeightFirst = +($('.floor-height-first').val());
-				var floorHeightSecond = +($('.floor-height_second').val());
-				var floorHeightThird = +($('.floor-height_third').val());
-				var lenght = +($('.input-3').val());
-				var width = +($('.input-2').val());
+	//от количества этажей появляются поля
+	floorCount.on('change', function() {
+		var result = $(this).val();
+		if(result == 1) {
+			absouluteFloor2.css('display', 'none');
+			absouluteFloor3.css('display', 'none');
+		}
+		else if(result == 2) {
+			absouluteFloor2.css('display', 'block');
+		}
+		else if(result == 3) {
+			absouluteFloor2.css('display', 'block');
+			absouluteFloor3.css('display', 'block');
+		}
+	});
+	//обработчик клика расчета
+	$(".calculator_btn").on("click", function (){
+		var houseType = $('input[name="type"]:checked').val();
+		if(houseType == 'type-1') {
+			var floorHeightFirst = +($('.floor-height-first').val());
+			var floorHeightSecond = +($('.floor-height_second').val());
+			var floorHeightThird = +($('.floor-height_third').val());
+			var lenght = +($('.input-3').val());
+			var width = +($('.input-2').val());
 
-				var blockAngular = ((floorHeightFirst + floorHeightSecond + floorHeightThird)/0.2) * 4;
-				console.log(blockAngular);
-
-			}
-			else if (houseType == 'type-2') {
-			}
-		});
+			//угловой блок
+			var blockAngular = ((floorHeightFirst + floorHeightSecond + floorHeightThird)/0.2) * 4;
+			console.log("блок угловой: " + blockAngular);
+			//периметр
+			perimetrTypeFirst = (lenght + width) * 2;
+			console.log("per: " + perimetrTypeFirst);
+			//поясной блок
+			var test  = perimetrTypeFirst - 3.2;
+			console.log(test);
+			console.log(floorCount);
+			var blockWaist = ((+perimetrTypeFirst - 3.2)/0.4) * (+floorCount);
+			console.log("блок поясной: " +  blockWaist);
+		}
+		else if (houseType == 'type-2') {
+		}
+	});
 
 		
 
